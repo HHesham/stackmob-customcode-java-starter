b9f6dc99ed92089adc84c8246869dd019ec56b83
HHesham <ahmed.hesham34@gmail.com>
2013-08-25 18:45:36 +0300

    Update HelloWorld.java

--- src/main/java/com/stackmob/customcode/HelloWorld.java
+++ src/main/java/com/stackmob/customcode/HelloWorld.java
@@ -14,43 +14,36 @@
  * limitations under the License.
  */
 
-package com.stackmob.customcode;
+package com.stackmob.example;
 
-import com.stackmob.core.InvalidSchemaException;
-import com.stackmob.core.DatastoreException;
 import com.stackmob.core.customcode.CustomCodeMethod;
 import com.stackmob.core.rest.ProcessedAPIRequest;
 import com.stackmob.core.rest.ResponseToProcess;
-import com.stackmob.example.Util;
 import com.stackmob.sdkapi.SDKServiceProvider;
-import com.stackmob.sdkapi.*;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import java.net.HttpURLConnection;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class HelloWorld implements CustomCodeMethod {
- 
+
   @Override
-  public String getMethodName() { 
-    return "Contact"; //no dashes or spaces allowed
+  public String getMethodName() {
+    return "hello_world";
   }
- 
+
   @Override
-  public List<String> getParams() { return new ArrayList<String>(); }
- 
+  public List<String> getParams() {
+    return new ArrayList<String>();
+  }
+
   @Override
   public ResponseToProcess execute(ProcessedAPIRequest request, SDKServiceProvider serviceProvider) {
     Map<String, Object> map = new HashMap<String, Object>();
     map.put("msg", "Hello, world!");
     return new ResponseToProcess(HttpURLConnection.HTTP_OK, map);
   }
- 
 
 }
